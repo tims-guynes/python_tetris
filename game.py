2b5d9495c9348a2f65630ec7cdf9b384058eef4d
Tims Guynes <tims.guynes@gmail.com>
2024-06-14 12:57:58 -0700

    update main code

---
+++ game.py
@@ -0,0 +1,52 @@
+from settings import *
+
+class Game:
+
+    def __init__(self) -> None:
+        #general
+        self.surface = pygame.Surface((GAME_WIDTH, GAME_HEIGHT)) #auto
+        self.display_surface = pygame.display.get_surface()
+        self.rect = self.surface.get_rect(topleft = (PADDING, PADDING))
+        self.sprites = pygame.sprite.Group()
+
+        #lines
+        self.line_surface = self.surface.copy()
+        self.line_surface.fill((0, 255, 0))
+        self.line_surface.set_colorkey((0, 255, 0))
+        self.line_surface.set_alpha(120)
+
+        #test
+        self.block = Block(self.sprites, (2,4),'blue')
+
+    def draw_grid(self):
+        for col in range(1, COLUMNS):
+            x = col * CELL_SIZE
+            pygame.draw.line(self.line_surface, LINE_COLOR, (x,0), (x, self.surface.get_height()), 1)
+        for row in range(1, ROWS):
+            y = row * CELL_SIZE
+            pygame.draw.line(self.line_surface, LINE_COLOR, (0,y), (self.surface.get_width(), y), 1)
+
+        self.surface.blit(self.line_surface, (0,0))
+
+    def run(self):
+
+        #drawing
+        self.surface.fill(GREY)
+        self.sprites.draw(self.surface)
+
+        self.draw_grid()
+        self.display_surface.blit(self.surface, (PADDING,PADDING))
+        pygame.draw.rect(self.display_surface, LINE_COLOR, self.rect, 2, 2)
+
+class Block(pygame.sprite.Sprite):
+    def __init__(self, group, pos, color):
+        
+        #general
+        super().__init__(group)
+        self.image = pygame.Surface((CELL_SIZE, CELL_SIZE))
+        self.image.fill(color) #temp
+
+        #position
+        x = pos[0] * CELL_SIZE
+        y = pos[1] * CELL_SIZE
+        self.rect = self.image.get_rect(topleft = (x,y))
--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 from settings import *
+from sys import exit
+
+#components
+from game import Game
+from score import Score
+from preview import Preview
 
 class Main:
     def __init__(self) -> None:
@@ -6,15 +12,33 @@ class Main:
         #general
         pygame.init()
         self.display_surface = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT))
+        pygame.display.set_caption('Tetris-clone') #game title
+        self.clock = pygame.time.Clock()
+
+        #components
+        self.game = Game()
+        self.score = Score()
+        self.preview = Preview()
 
     def run(self):
+        #exit program
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     exit()
+            
             #display
+            self.display_surface.fill(GREY)
+
+            #components
+            self.game.run()
+            self.score.run()
+            self.preview.run()
+            
+            #update gaem
             pygame.display.update()
+            self.clock.tick(60)
 
 if __name__ == '__main__':
     main = Main()
